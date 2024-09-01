db68c41a18f1c9b0876898b22c19f5032a70a181
Vitaly <ivanov.arduan@yandex.ru>
2024-09-01 19:46:20 +0300

    My second commit.

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,4 +5,7 @@ from .views import upload_profile, profile_list
 urlpatterns = [
     path('upload/', upload_profile, name='upload_profile'),
     path('', profile_list, name='profile_list'),
+    path('upload/', upload_profile, name='upload_profile'),
+
+
 ]
